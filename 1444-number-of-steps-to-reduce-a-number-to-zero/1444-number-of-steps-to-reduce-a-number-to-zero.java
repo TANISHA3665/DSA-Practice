6cacec4e48339240ca072199d39725cffe070dcf
TANISHA JAISWAL <jaiswaltanisha11@gmail.com>
2025-01-16 17:15:57 +0530

    Time: 0 ms (100%), Space: 40.2 MB (95.2%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,18 +1,18 @@
 class Solution {
     public int numberOfSteps(int num) {
+        if(num == 0){
+            return 0;
+        }
+        
         int steps = 0;
 
-        while(num > 0){
-            if(num % 2 == 0){
-                num /= 2;
-            }
-            else {
-                num--;
-            }
+        while(num != 0){
+            
+            steps += (num & 1) == 0 ? 1 : 2;
 
-            steps++;
+            num >>= 1; 
         }
 
-        return steps;
+        return steps - 1;
     }
 }
